cfe32aec16ca399dd854a57954f0a90fda92a87a
KingWeener <156138523+KingWeener@users.noreply.github.com>
2024-02-25 15:54:02 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
- 
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    info.changeScoreBy(1)
+    sprites.destroy(mySprite, effects.spray, 500)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += -100
+})
+let mySprite: Sprite = null
+tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 11))
+info.setScore(0)
+mySprite = sprites.create(img`
+    f f f f f f f f f f f f f f f f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 f f f 1 1 f f f 1 1 1 f 
+    f 1 1 1 f f f 1 1 f f f 1 1 1 f 
+    f 1 1 1 f f f 1 1 f f f 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 f f f f f f f f f f 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+    f f f f f f f f f f f f f f f f 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+let gravity = 100
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite.ay = gravity
+mySprite.setVelocity(50, 0)
